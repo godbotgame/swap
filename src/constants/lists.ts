d303c7874416fe34d09309cba074325ae93c05d9
gasonzhong <gasonzhong@163.com>
2022-01-24 11:52:13 +0800

    webs

--- src/constants/lists.ts
+++
@@ -1,4 +0,0 @@
-// the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://tokens.bscswap.com/tokens.json'
-
-export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
